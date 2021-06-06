aba0692bbd474807d5bfefb5d3fcd5e1cd14285a
Mohamad Aziz <mohamad.aziz@dugi-indonesia.com>
2021-06-06 20:33:45 +0700

    initial commit

---
+++ database/factories/FollowUpFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\FollowUp;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class FollowUpFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = FollowUp::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_06_06_132702_create_follow_ups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowUpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follow_ups', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("agent_id");
+            $table->foreign("agent_id")->references("id")->on("users")->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger("customer_id");
+            $table->foreign("customer_id")->references("id")->on("customers")->onUpdate('cascade')->onDelete('cascade');
+            $table->string("message");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follow_ups');
+    }
+}
